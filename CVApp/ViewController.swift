bf36251e71ae88d4c837984b4acfb46189f7eab3
Ricardo Moreno <ricardo.moreno@globant.com>
2019-11-21 12:29:11 -0600

    Initial Commit

---
+++ CVApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CVApp
+//
+//  Created by Ricardo Moreno Martinez on 11/21/19.
+//  Copyright © 2019 Ricardo Moreno Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
